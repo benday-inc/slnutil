621fcaa4b2d4173adc8e368fc62374b3958750a4
Benjamin Day <benday@benday.com>
2023-03-03 13:18:40 -0500

    Implemented replace token in file command

--- Benday.SolutionUtil.Api/Constants.cs
+++ Benday.SolutionUtil.Api/Constants.cs
@@ -19,6 +19,8 @@ public static class Constants
     public const string CommandArgumentNameGetConnectionString = "getconnectionstring";
     public const string CommandArgumentNameSetJsonValue = "setjsonvalue";
 
+    public const string CommandArgumentNameReplaceToken = "replacetoken";
+    public const string ArgumentNameToken = "token";
 
 
     public const string ArgumentNameValue = "value";
---
+++ Benday.SolutionUtil.Api/ReplaceTokenCommand.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+
+using Benday.CommandsFramework;
+namespace Benday.SolutionUtil.Api;
+
+[Command(Name = Constants.CommandArgumentNameSetJsonValue,
+    Description = "Replace token in file.")]
+public class ReplaceTokenCommand : SynchronousCommand
+{
+    public ReplaceTokenCommand(CommandExecutionInfo info, ITextOutputProvider outputProvider) :
+        base(info, outputProvider)
+    {
+
+    }
+
+    public override ArgumentCollection GetArguments()
+    {
+        var args = new ArgumentCollection();
+
+        args.AddString(Constants.ArgumentNameConfigFilename)
+            .AsRequired()
+            .WithDescription("Path to file");
+
+        args.AddString(Constants.ArgumentNameToken)
+            .AsRequired()
+            .WithDescription("Token to replace");
+
+        args.AddString(Constants.ArgumentNameValue)
+            .AsRequired()
+            .WithDescription("String value to set");
+
+        return args;
+    }
+
+    protected override void OnExecute()
+    {
+        var configFilename = Arguments.GetStringValue(Constants.ArgumentNameConfigFilename);
+        Utilities.AssertFileExists(configFilename, Constants.ArgumentNameConfigFilename);
+
+        var configToken = Arguments.GetStringValue(Constants.ArgumentNameToken);
+        var configValue = Arguments.GetStringValue(Constants.ArgumentNameValue);
+
+        var text = File.ReadAllText(configFilename);
+
+        if (text.Contains(configToken) == true)
+        {
+            text = text.Replace(configToken, configValue);
+
+            File.WriteAllText(configFilename, text);
+        }
+    }
+
+    protected void AssertFileExists(string path, string argumentName)
+    {
+        if (File.Exists(path) == false)
+        {
+            var info = new FileInfo(path);
+
+            string message = String.Format(
+                "File for argument '{0}' was not found at '{1}'.",
+                argumentName,
+                info.FullName);
+
+            throw new FileNotFoundException(
+                message, path);
+        }
+    }
+}
